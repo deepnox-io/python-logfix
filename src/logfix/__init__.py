1596d372c618f992f5034b7ebb010305e47f02db
François Michaud <fmichaud@deepnox.io>
2021-05-06 14:00:23 +0200

    Improve log fix function, add func to fix files into a directory

--- src/logfix/__init__.py
+++ src/logfix/__init__.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-
+import glob
+import logging
 import os
 import re
-import loggz
-import uuid
 
 JAVA_STACKTRACE_PATTERN = '^.*[\r\n]+.*?Exception.*(?:[\r\n]+^\s*at .*)+'
 JAVA_STACKTRACE_REGEX = re.compile(JAVA_STACKTRACE_PATTERN, re.MULTILINE)
 
-MAIN_LOG = loggz.factory('log-repair')
+MAIN_LOG = logging.getLogger('logfix')
 """ The main logger. 
 """
 
@@ -24,20 +23,37 @@ def _replace(string, substitutions):
     return regex.sub(lambda match: substitutions[match.group(0)], string)
 
 
-def repair(filename, tmp_file: str):
-    """ Repair a log file.
+def file_fix(src: str, dst: str):
+    """ Repair a logfile.
+    :param src: The file to fix.
+    :param dst: Output file to store fixed logs.
+
     """
-    MAIN_LOG.debug('repair_log_file()')
+    MAIN_LOG.debug(f'file_fix(src={src},dst={dst})')
+    if not src:
+        raise Exception('Missing source file to fix.')
+    if not dst or len(dst) == 0:
+        dst = f'{dst}.fixed'
     stacks = {}
-    if tmp_file is None:
-        tmp_file = os.path.join(os.path.dirname(filename), uuid.uuid4())
     rx_blanks = re.compile(r'\W+')  # to remove blanks and newlines
-    file_source = open(filename, 'r')
+    file_source = open(src, 'r')
     lines = '\n'.join(file_source.readlines())
     for match in JAVA_STACKTRACE_REGEX.finditer(lines):
         stacks[match.group(0)] = match.group(0).replace('\n', '\\n').replace('\t', ' ')
 
     s = _replace(lines, stacks)
-    dst = open(tmp_file, 'w')
+    dst = open(dst, 'w')
     dst.write(s)
     dst.close()
+
+
+def directory_fix(dir: str, file_pattern: str = '*'):
+    """ Fix logs files founded into the specified directory.
+
+    :param dir: The directory containing logs files.
+    """
+    MAIN_LOG.debug(f'directory_fix(dir={dir}, file_pattern={file_pattern}')
+    if not dir or not os.path.exists(dir):
+        raise Exception(f'Path {dir} does not exist')
+    for file in glob.glob(os.path.join(dir), file_pattern):
+        file_fix(file)
