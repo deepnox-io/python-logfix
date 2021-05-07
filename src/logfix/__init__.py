20b70c36e02742111c79912dc6944599655d41b5
François Michaud <ext.francois.michaud@sncf.fr>
2021-05-07 09:46:52 +0200

    Return fixed filenames

--- src/logfix/__init__.py
+++ src/logfix/__init__.py
@@ -3,11 +3,12 @@ import glob
 import logging
 import os
 import re
+from pathlib import Path
 
 JAVA_STACKTRACE_PATTERN = '^.*[\r\n]+.*?Exception.*(?:[\r\n]+^\s*at .*)+'
 JAVA_STACKTRACE_REGEX = re.compile(JAVA_STACKTRACE_PATTERN, re.MULTILINE)
 
-MAIN_LOG = logging.getLogger('logfix')
+LOG = logging.getLogger('logfix')
 """ The main logger. 
 """
 
@@ -17,23 +18,23 @@ def _replace(string, substitutions):
 
     :ref: https://gist.github.com/carlsmith/b2e6ba538ca6f58689b4c18f46fef11c
     """
-    MAIN_LOG.debug('replace()')
+    LOG.debug('replace()')
     substrings = sorted(substitutions, key=len, reverse=True)
     regex = re.compile('|'.join(map(re.escape, substrings)))
     return regex.sub(lambda match: substitutions[match.group(0)], string)
 
 
-def file_fix(src: str, dst: str):
+def file_fix(src: str, dst: str = None):
     """ Repair a logfile.
     :param src: The file to fix.
     :param dst: Output file to store fixed logs.
 
     """
-    MAIN_LOG.debug(f'file_fix(src={src},dst={dst})')
+    LOG.debug(f'file_fix(src={src},dst={dst})')
     if not src:
         raise Exception('Missing source file to fix.')
     if not dst or len(dst) == 0:
-        dst = f'{dst}.fixed'
+        dst = f'{src}.fixed'
     stacks = {}
     rx_blanks = re.compile(r'\W+')  # to remove blanks and newlines
     file_source = open(src, 'r')
@@ -45,15 +46,23 @@ def file_fix(src: str, dst: str):
     dst = open(dst, 'w')
     dst.write(s)
     dst.close()
+    return dst
 
 
-def directory_fix(dir: str, file_pattern: str = '*'):
+def directory_fix(directory: str, file_pattern: str = '*'):
     """ Fix logs files founded into the specified directory.
 
-    :param dir: The directory containing logs files.
+    :param directory: The directory containing logs files.
+    :param file_pattern: The pattern to match files.
     """
-    MAIN_LOG.debug(f'directory_fix(dir={dir}, file_pattern={file_pattern}')
-    if not dir or not os.path.exists(dir):
-        raise Exception(f'Path {dir} does not exist')
-    for file in glob.glob(os.path.join(dir), file_pattern):
+    LOG.debug(f'directory_fix(directory={directory}, file_pattern={file_pattern}')
+    fixed_files = []
+    if not directory or not os.path.exists(directory):
+        raise Exception(f'Path {directory} does not exist')
+    p = Path(directory)
+    LOG.debug('files', extra={'files': [str(f) for f in p.glob('*')]})
+    for file in p.glob('*'):
+        LOG.debug(str(file))
         file_fix(file)
+        fixed_files.append(file)
+    return fixed_files
